816881a3e4762b5dd004841fa795f32234ceefc0
misko9655 <milosmilutinovicwork@gmail.com>
2026-01-29 14:03:23 +0000

    * added code for customer controller and service

--- server/src/customers/customers.controller.ts
+++ server/src/customers/customers.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Customer } from "../../../shared/customer"
+import { CustomersService } from "./customers.service";
 
 
 @Controller('customers')
 export class CustomersController {
+    constructor(private customerDB: CustomersService) {}
+    
+    @Post() 
+    async createCustomer(@Body() customer: Partial<Customer>): Promise<Customer> {
+        return this.customerDB.createCustomer(customer);
+    }
+
+    @Get()
+    async getAllCustomers(): Promise<Customer[]> {
+        console.log('Fetching all norms from DB');
+        return this.customerDB.getAllCustomers();
+    }
+
+    @Put(':customerId')
+    async updateCustomer(@Param() customerId: string, @Body() changes: Partial<Customer>): Promise<Customer | null> {
+        return this.customerDB.updateCustomer(customerId, changes);
+    }
+
+    @Delete(':customerId')
+    async deleteCustomer(@Param() customerId: string) {
+        this.customerDB.deleteCustomer(customerId);
+    }
 
 }
--- server/src/customers/customers.service.ts
+++ server/src/customers/customers.service.ts
@@ -1,6 +1,29 @@
+import { InjectModel } from "@nestjs/mongoose";
+import { Customer } from "../../../shared/customer";
+import { Model } from "mongoose";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class CustomersService {
+    constructor(@InjectModel('Customer') private customerModel: Model<Customer>) {}
+
+    deleteCustomer(customerId: string): Promise<Customer | null> {
+       return this.customerModel.findByIdAndDelete(customerId).exec();
+    }
+    updateCustomer(customerId: string, changes: Partial<Customer>): Promise<Customer | null> {
+        return this.customerModel.findOneAndUpdate(
+            { _id: customerId},
+            changes,
+            {new: true}
+        )
+    }
+    async getAllCustomers(): Promise<Customer[]> {
+        return this.customerModel.find();
+    }
+    async createCustomer(customerData: Partial<Customer>): Promise<Customer> {
+        const newCustomer = new this.customerModel(customerData);
+        await newCustomer.save();
+        return newCustomer.toObject({versionKey: false});
+    }
 
 }
